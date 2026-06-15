5612c5de6e7a3653f7a19b23973c74b89b45883f
anshpatel0425 <anshpatel4625@gmail.com>
2026-06-15 11:26:39 +0000

    scope and mini hoisting

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -22,3 +22,62 @@ if (true){
     
 }
 console.log(myName); /*declared outside the block will act as a global scope */
+
+// nested scope
+function one(){
+    const username = "Patel"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);  /*website is not accesiable because it's scope is ended in function two */
+    
+    two()
+}
+one()
+
+
+// errors in if else scope
+if(true){
+    const username = "RISH"
+    if(username === "RISH"){
+        const website = " website"
+        console.log(username + website);
+        
+    }
+    // console.log(website); this will give errors 
+    
+}
+// console.log(username); this also gives errors
+
+
+// ++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+function addOne(num){
+    return num + 1
+}
+addOne(5) /*this will not print any result just returns the result because we didn't used console log */
+
+// another way
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5) /*this will not print any result just returns the result because we didn't used console log */
+
+
+// complex
+
+console.log(addThree(5)); /*using function before declartion is valid */
+
+
+function addThree(num){
+    return num + 3
+}
+
+console.log(addFour(5)); /*this will give error because it doesn't allowed in variable to use it before declartion */
+
+const addFour = function(num){
+    return num + 4
+}
